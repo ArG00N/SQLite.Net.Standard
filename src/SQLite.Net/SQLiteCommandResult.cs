cc7df62358c523d803aa44431eadd27851121dbe
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-01 11:44:16 +1000

    Include column names in the result - regardless of whether or not any data was actually selected

---
+++ src/SQLite.Net/SQLiteCommandResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SQLite.Net
+{
+    public class SQLiteCommandResult
+    {
+        public Collection<Dictionary<string, object>> Data { get; } = new Collection<Dictionary<string, object>>();
+        public Collection<string> ColumnNames { get; set; } = new Collection<string>();
+    }
+}
