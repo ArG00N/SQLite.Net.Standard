98a702fb94bfa59892e7e4093fcfb7bcbc71ebef
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-17 17:05:18 +1000

    Delete unnecessary stuff.

--- nuget/build.bat
+++
@@ -1,4 +0,0 @@
-@mkdir output
-..\.nuget\nuget pack SQLite.Net.Core.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.nuspec -o output
-..\.nuget\nuget pack SQLite.Net.Async.nuspec -o output
--- nuget/upload.bat
+++
@@ -1,3 +0,0 @@
-..\.nuget\nuget push output\SQLite.Net.Core-PCL.3.1.1.nupkg
-..\.nuget\nuget push output\SQLite.Net-PCL.3.1.1.nupkg
-..\.nuget\nuget push output\SQLite.Net.Async-PCL.3.1.1.nupkg
