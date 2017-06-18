2165cf5b3196d2d7b1352d1954404fa671275987
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-18 18:36:16 +1000

    Add android project.

--- examples/StocksAndroid/MainActivity.cs
+++ examples/StocksAndroid/MainActivity.cs
@@ -2,14 +2,12 @@
 
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using System.IO;
 
 namespace XamForms.Droid
 {
-	[Activity (Label = "XamForms", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity (Label = "XamForms", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
@@ -17,7 +15,13 @@ namespace XamForms.Droid
 			base.OnCreate (bundle);
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new XamForms.App ());
+
+
+            var app = new XamForms.App();
+            app.DBPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "stocks.db");
+            //app.SQLitePlatform = new SQ
+
+            LoadApplication (app);
 		}
 	}
 }
